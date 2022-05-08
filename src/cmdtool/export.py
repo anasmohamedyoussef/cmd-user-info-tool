5f8c24f3be4f3bfa5b4e5b905b0543bb02b5aa89
anasmohamedyoussef <anasmohamedyoussef@gmail.com>
2022-05-08 18:38:34 +0200

    get info in csv format fix

--- src/cmdtool/export.py
+++ src/cmdtool/export.py
@@ -7,7 +7,7 @@ def toJson(export_file, users):
 
 def toCSV(export_file, users):
     export_file.write('name,id,home,shell\n')
-    writer = csv.write(export_file)
+    writer = csv.writer(export_file)
     rows= [[user['name'],user['id'],user['home'],user['shell']] for user in users]
     writer.writerows(rows)
     export_file.close()
